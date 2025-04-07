d429e30123219757a2c3eeac1edcaada1c628dea
yogit4554 <yt7407286@gmail.com>
2025-04-07 18:07:15 +0530

    first commit

---
+++ server/src/main/java/com/yteewari/fullStakProject/FullStakProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yteewari.fullStakProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStakProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStakProjectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/yteewari/fullStakProject/FullStakProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yteewari.fullStakProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStakProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
